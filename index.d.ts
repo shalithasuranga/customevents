33d874cfa7dfcaaa76dfbbe5c4f66f64df79d213
Shalitha Suranga <shalithasuranga@gmail.com>
2021-08-03 22:56:34 +0530

    Adding initial code

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export function on(eventName: string, handler: any): void; 
+export function off(eventName: string, handler: any): void; 
+export function dispatch(eventName: string, data: any): void; 
---
+++ index.js
@@ -0,0 +1,19 @@
+function on(eventName, handler) {
+	window.addEventListener(eventName, handler);
+}
+
+function off(eventName, handler) {
+	window.removeEventListener(eventName, handler);
+}
+
+function dispatch(eventName, data) {
+	window.dispatchEvent(
+		new CustomEvent(eventName, {detail: data})
+	);
+}
+
+module.exports = {
+	on,
+	off,
+	dispatch
+};
