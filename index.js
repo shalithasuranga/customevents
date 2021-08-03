c273df6fb69bf70520df168e5bbb11a76160e7b7
Shalitha Suranga <shalithasuranga@gmail.com>
2021-08-03 23:03:26 +0530

    Some code formatting

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function on(eventName, handler) {
-	window.addEventListener(eventName, handler);
+    window.addEventListener(eventName, handler);
 }
 
 function off(eventName, handler) {
-	window.removeEventListener(eventName, handler);
+    window.removeEventListener(eventName, handler);
 }
 
 function dispatch(eventName, data) {
-	window.dispatchEvent(
-		new CustomEvent(eventName, {detail: data})
-	);
+    window.dispatchEvent(
+        new CustomEvent(eventName, {detail: data})
+    );
 }
 
 module.exports = {
-	on,
-	off,
-	dispatch
+    on,
+    off,
+    dispatch
 };
